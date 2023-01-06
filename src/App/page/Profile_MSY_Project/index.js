cce6c2cfe539942e18a57a44bb839da36d27038a
xuandien24111997 <hoangxuandien24111997@gmail.com>
2023-01-06 11:56:55 +0700

    change theme 4

--- src/App/page/Profile_MSY_Project/index.js
+++ src/App/page/Profile_MSY_Project/index.js
@@ -6,7 +6,7 @@ import Theme4 from "./Theme4";
  
 
 const ThemeChoose = () => {
-  const [chooseTheme, setChooseTheme] = useState("info-template-3")
+  const [chooseTheme, setChooseTheme] = useState("info-template-4")
  
   const data = [
     {
